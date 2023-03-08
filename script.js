a4c1bbe9ed11526146f322806562d6471577cf8c
halil <halilerk77@icloud.com>
2023-03-08 01:19:19 +0100

    finished

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const quizData = [
   {
-    question: "How old is Florin?",
-    a: "10",
-    b: "17",
-    c: "26",
-    d: "110",
-    correct: "c",
-  },
-  {
-    question: "What is the most programming language in 2019",
+    question: "What is the most used programming language in 2019?",
     a: "Java",
     b: "C",
     c: "Python",
@@ -16,19 +8,19 @@ const quizData = [
     correct: "d",
   },
   {
-    question: "Who is the President of Us in 2023",
+    question: "Who is the President of US?",
     a: "Florin Pop",
-    b: "John Biden",
-    c: "Trump Donald",
-    d: "Barrack Obama",
+    b: "Donald Trump",
+    c: "Ivan Saldano",
+    d: "Mihai Andrei",
     correct: "b",
   },
   {
-    question: "What does HTML stand for",
+    question: "What does HTML stand for?",
     a: "Hypertext Markup Language",
     b: "Cascading Style Sheet",
     c: "Jason Object Notation",
-    d: "Application Programming Interface",
+    d: "Helicopters Terminals Motorboats Lamborginis",
     correct: "a",
   },
   {
@@ -41,30 +33,68 @@ const quizData = [
   },
 ];
 
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
-
+console.log(answerEls[0]);
 let currentQuiz = 0;
+let score = 0;
+
 loadQuiz();
 
 function loadQuiz() {
+  deselectAnswers();
+
   const currentQuizData = quizData[currentQuiz];
-  questionEl.innerHTML = currentQuizData.question;
-  a_text.innerHTML = currentQuizData.a;
-  b_text.innerHTML = currentQuizData.b;
-  c_text.innerHTML = currentQuizData.c;
-  d_text.innerHTML = currentQuizData.d;
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+  let answer = undefined;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
 }
 
 submitBtn.addEventListener("click", () => {
-  currentQuiz++;
-  if (currentQuiz < quizData.length) {
-    loadQuiz();
-  } else {
-    alert("no left question");
+  // check to see the answer
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+              <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+              
+              <button onclick="location.reload()">Reload</button>
+          `;
+    }
   }
 });
